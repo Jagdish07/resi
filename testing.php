248962e0477ff6ecb26311890f2b4c09e301ad3f
Jagdish07 <bhakuniijagdish07@gmail.com>
2018-08-09 16:44:21 +0530

    Create testing.php

---
+++ testing.php
@@ -0,0 +1,37 @@
+" Hazel Grain "
+Dear Client,
+Our development timings are from 9:30am to 7:00pm IST ( GMT +5:30 ). However we are available for more than 14 hours for client communications and project status updates.
+
+
+> You can reach us on skype at " help.adisus "
+
+
+Our Previous work with Amazon Seller APIs :-
+Project Name :- PrioTicket
+Web-Url :- https://prioticket.com/
+APIs Used :-
+We have used Amazon SNS and SQS APIs in this project.
+
+We are pretty sure we can deliver what this project requires. However it would be great if we can schedule a meeting to discuss more about project specifications so that we can understand your requirements better and provide you best solution for it.
+
+***** Who We Are ? *****
+We are the IT Service Provider Company Known to deliver ​complex​ ​Enterprise​ ​software​ ​solutions​ ​across​ industries​ ​and​ ​functions​ ​from Business​ ​Process​ ​Automation,​ ​ERP​ ​for Maintenance,​ ​repair​ ​and​ ​overhaul​ ​aviation​ ​sector,​ ​work-flow automation​ ​and​ ​advanced​ ​reporting​ ​tools.
+
+***** Our Process *****
+We work in process driven manner. Instead of just started working on a task just to get that completed straightway we first analyse it and then plan the solution accordingly which saves a lots of time.
+Not only this we also check if there is a better way to solve a certain problem then we recommend that first to client before working on it.
+
+We usually don't apply temporary solutions or workarounds for a problem but if it comes to that for some reason we make sure to mark some kind of maker or TO-DO comment so that it doesn't get lost and can be traced again to get it fixed properly.
+
+We understand that requirement keep on changing so we make sure we incorporate the change requests in best possible manner. Following Agile methodologies makes it easy to entertain new change request
+
+***** KEY TO OUR ON TIME DELIVERY OF PROJECT WITH QUALITY *****
+The core of our development is agile scrum. Almost all our major projects are done using Agile methodology. Sprints are designed and worked upon in a dynamic manner where our project lead and client meet frequently to design and review sprints and changes etc.
+We use Redmine (Project Management tool) where project manager assigns task to all the developers involved in some particular project. Developers update the status there of their tasks i.e still working on/ completed or need feedback from someone. Tester tests the work done by developers and then update it in Redmine and developers solve it. Clients can see everything happening during the project work and can also update if they feel any change in work or any bug or anything they feel like updating to team.
+
+If you have any query, please feel free to ask. We would love to solve all your queries and provide you the better services as well. We believe in delivering excellence hence we will use all best of our knowledge and skills in order to make sure of this.
+
+Hope to hear from you soon !!
+
+Best Regards.
+Deepika Sharma.
